b17354e2e14baa94561ee9eb5830278881c5b345
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-01-11 14:06:18 -0500

    commit tree 14dfb5a5efc29d88374b82dc4a55ac22481c1fd3

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+    def breed=(given_breed)
+        @breed = given_breed
+    end
+
+    def breed
+        @breed
+    end
+
+    def name=(given_name)
+        @name = given_name
+    end
+
+    def name
+        @name
+    end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person 
+    def job=(given_job)
+        @job = given_job
+    end
+
+    def job
+        @job
+    end
+
+    def name=(given_name)
+        @name = given_name
+    end
+
+    def name
+        @name
+    end
+
+end
